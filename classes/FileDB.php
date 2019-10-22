581edf63aae9c092be4cb6592fb51513801aa08b
dana1031 <dana1031@gmail.com>
2019-10-22 12:42:56 +0300

    FileDB

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -1,13 +1,22 @@
 <?php
 
-
-
 class FileDB {
+
     private $file_name;
-       
+    
+    /** @var $data array Duomenu masyvas */
+    private $data;
+
+    /**
+     * F-ija, kuri issikviecia sukurus objekta
+     * @param type $file_name Failo pavadinimas
+     */    
     public function __construct($file_name) {
         $this->file_name = $file_name;
-    }  
-}
-var_dump($file_name);
+    }
 
+    public function load() {
+        $this->data = file_to_array($this->file_name);
+    }
+
+}
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+require 'function/file.php';
 require 'classes/FileDB.php';
 
 $db = new FileDB('failas.txt');
