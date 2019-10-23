77f8d3e229914e5f76d43da6a0fd14f5718a5ce8
dana1031 <dana1031@gmail.com>
2019-10-23 11:25:53 +0300

    if exist Table

--- classes/FileDB.php
+++ classes/FileDB.php
@@ -1,39 +1,45 @@
 <?php
 
 class FileDB {
-   private $file_name;
-   private $data;
-   public function __construct($file_name) {
-       $this->file_name = $file_name;
-   }
-   public function load() {
-       $this->data = file_to_array($this->file_name);
-   }
-   public function setData($data_array) {
-       $this->data = $data_array;
-   }
-   public function save() {
-      return array_to_file($his->data, $this->file_name);
-   }
-   public function getData() {
-       return $this->data;
-   }
-  public function createTable($table_name) {
-      if (!isset($this->data[$table_name])) {
-          $this->data[$table_name] = [];
-      }
-      return false;
-  } 
-  public function tableExists($table_name){
-      if (isset ($this->data[$table_name])){
-          return  true;
-      }    
-              else false;
-      }
-  }
-    
 
-  
+    private $file_name;
+    private $data;
 
+    public function __construct($file_name) {
+        $this->file_name = $file_name;
+    }
 
+    public function load() {
+        $this->data = file_to_array($this->file_name);
+    }
 
+    public function setData($data_array) {
+        $this->data = $data_array;
+    }
+
+    public function save() {
+        return array_to_file($his->data, $this->file_name);
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+    public function tableExists($table_name) {
+        if (isset($this->data[$table_name])) {
+            return true;
+        } else
+            false;
+    }
+
+    public function createTable($table_name) {
+        if (tableExists($table_name)) {
+            return true;
+        } else 
+            {
+            $this->data[$table_name] = [];
+        }
+      
+    }
+
+}
